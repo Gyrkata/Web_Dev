e5c329f1a63c19157672ff396576263af7e73b76
Gyrkata <nikolyog@gmail.com>
2022-05-01 12:58:36 +0100

    added static page handler, added routes and errors

--- controllers/restaurant_menu.js
+++ controllers/restaurant_menu.js
@@ -10,6 +10,22 @@ router.get('/guestbook', function(req, res){
 res.send('<h1>Have a look at our menu!</h1>')
 })
 
+router.get('/about', function(req, res) {
+    res.redirect('/about.html');
+})
+
+router.use(function(req, res){
+    res.status(404);
+    res.type('text/plain');
+    res.send('404 Not Found');
+})
+
+router.use(function(req, res){
+    res.status(500);
+    res.type('text/plain');
+    res.send('Internal Server Error');
+})
+
 
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+
+const path = require('path');
+const public = path.join(_dirname,'public');
+app.use(express.static(public));
+
+app.use(express.urlencoded({extended: false}));
+
 const router = require('./controllers/restaurant_menu');
 app.use('/', router);
 
