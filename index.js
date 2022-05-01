fcb102f13c4cd07f82058a53416ffa3bdb3a316c
Gyrkata <nikolyog@gmail.com>
2022-05-01 11:38:26 +0100

    added controller for menu routes

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res){
-res.send('Welcome! Have a look at our menu!');
-})
+const router = require('./controllers/restaurant_menu');
+app.use('/', router);
 
 app.listen(3000, () => {
 console.log('Server started on port 3000. Ctrl^c to quit');
