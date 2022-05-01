41e63b48ad1716d78edfb655087416436db86ad4
Gyrkata <nikolyog@gmail.com>
2022-05-01 12:04:46 +0100

    installed path, added another route

--- controllers/restaurant_menu.js
+++ controllers/restaurant_menu.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", function(req, res) {
-  res.send('Welcome! Have a look at our menu');
+  res.send('Welcome to Restaurant Lapicheto');
 
 })
+
+router.get('/guestbook', function(req, res){
+res.send('<h1>Have a look at our menu!</h1>')
+})
+
+
 
 module.exports = router;
